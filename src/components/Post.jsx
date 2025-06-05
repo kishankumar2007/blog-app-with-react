308500d73d72aafad764e8e82e522bb97c85a204
mrperfectVip <kk7012234@gmail.com>
2025-06-05 18:03:47 +0530

    improvement done

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -8,27 +8,30 @@ import { toast } from 'react-toastify'
 
 function Post() {
   const [image, setImage] = useState(null)
+  const [postId, setpostId] = useState('')
   const { slug } = useParams()
   const navigate = useNavigate()
   const dispatch = useDispatch()
   const posts = useSelector(state => state.post.allPosts)
   const userData = useSelector(state => state.auth.userData)
-  const post = posts.find((post) => post.slug === slug) || null
+  const post = posts.find((post) => post.slug === slug)
 
 
   useEffect(() => {
-    if (post == null) {
-      setTimeout(() =>{ navigate('/')},500)
+    setpostId('')
+    if (!post) {
+      setTimeout(() => { navigate('/') }, 500)
       return
     }
-
+    const postOwnerId = post.$permissions[1].replace('update(\"user:', '').replace('")', '')
+    setpostId(postOwnerId)
     async function getImage() {
       const dbImg = await storageService.filePreview(post.Image)
       setImage(dbImg)
 
     }
     getImage()
-  }, [post])
+  }, [post, navigate])
 
 
   const deletePost = async () => {
@@ -78,7 +81,12 @@ function Post() {
 
   return (
     <div className='w-full min-h-screen sm:p-10 p-5'>
-      {post?.userId === userData?.$id && <div className='flex gap-3 top-18 sm:text-sm text-xs lg:right-50 right-2 absolute sm:right-15'><button onClick={editPost} className='rounded bg-emerald-500 text-white p-1 sm:p-2'>📝Edit</button> <button onClick={deletePost} className='rounded bg-red-600 text-white p-1 sm:p-2'> 🗑️ Delete</button></div>}
+      {(postId === userData?.userId || postId === userData?.$id) && (
+        <div className='flex gap-3 top-18 sm:text-sm text-xs lg:right-50 right-2 absolute sm:right-15'>
+          <button onClick={editPost} className='rounded bg-emerald-500 text-white p-1 sm:p-2'>📝Edit</button>
+          <button onClick={deletePost} className='rounded bg-red-600 text-white p-1 sm:p-2'>🗑️ Delete</button>
+        </div>
+      )}
       <div className='max-w-5xl mx-auto sm:h-xl flex mt-4 flex-col sm:flex-row sm:gap-30  items-center'> <div className='max-w-96 max-h-96 rounded-md overflow-hidden '> <img className='w-full object-cover ' src={image} alt='post_image' /> </div> <h1 className='titl sm:text-5xl text-xl mt-5 font-semibold uppercase shadow text-white'>{post?.title}</h1> </div>
       <h1 className=" max-w-5xl mx-auto mt-4 content text-center text-2xl text-white underline"> Content </h1>
       <div className=" max-w-5xl mx-auto mt-4 content text-white sm:text-lg text-sm font-light" dangerouslySetInnerHTML={{ __html: post?.content }} />
